710c120b7de99a514d713d61aa38784cbfbcde00
Juan Quezada <ju.quezada.c@gmail.com>
2021-08-30 12:52:33 -0400

    commit

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,13 +1,12 @@
-let Radiologia = [
+var Radiologia = [
     {hora: '11:00', especialista: 'IGNACIO SCHULZ', paciente: 'FRANCISCA ROJAS', rut: '9878782-1', prevision: 'FONASA'},
     {hora: '11:30', especialista: 'FEDERICO SUBERCASEAUX', paciente: 'PAMELA ESTRADA', rut: '15345241-3', prevision: 'ISAPRE'},
     {hora: '15:00', especialista: 'FERNANDO WURTHZ', paciente: 'ARMANDO LUNA', rut: '16445345-9', prevision: 'ISAPRE'},
     {hora: '15:30', especialista: 'ANA MARIA GODOY', paciente: 'MANUEL GODOY', rut: '17666419-0', prevision: 'FONASA'},
     {hora: '16:00', especialista: 'PATRICIA SUAZO', paciente: 'RAMON ULLOA', rut: '14989389-K', prevision: 'FONASA'},
 ];
-
-let Traumatologia = [
-    {hora: '8:00',  especialista: 'MARIA PAZ ALTUZARRA', paciente: 'PAULA SANCHEZ',     rut: '15554774-5', prevision: 'FONASA'},
+var Traumatologia = [
+    {hora: '08:00',  especialista: 'MARIA PAZ ALTUZARRA', paciente: 'PAULA SANCHEZ',     rut: '15554774-5', prevision: 'FONASA'},
     {hora: '10:00', especialista: 'RAUL ARAYA',          paciente: 'ANGÉLICA NAVAS',    rut: '15444147-9', prevision: 'ISAPRE'},
     {hora: '10:30', especialista: 'MARIA ARRIAGADA',     paciente: 'ANA KLAPP',         rut: '17879423-9', prevision: 'ISAPRE'},
     {hora: '11:00', especialista: 'ALEJANDRO BADILLA',   paciente: 'FELIPE MARDONES',   rut: '1547423-6', prevision: 'ISAPRE'},
@@ -15,14 +14,50 @@ let Traumatologia = [
     {hora: '12:00', especialista: 'ARTURO CAVAGNARO',    paciente: 'CECILIA MENDEZ',    rut: '9747535-8', prevision: 'ISAPRE'},
     {hora: '12:30', especialista: 'ANDRES KANACRI',      paciente: 'MARCIAL SUAZO',     rut: '11254785-5', prevision: 'ISAPRE'},
 ];
-let Dental = [
-    {hora: '8:30',  especialista: 'ANDREA ZUÑIGA',          paciente: 'MARCELA RETAMAL',rut: '11123425-6', prevision: 'ISAPRE'},
+var Dental = [
+    {hora: '08:30',  especialista: 'ANDREA ZUÑIGA',          paciente: 'MARCELA RETAMAL',rut: '11123425-6', prevision: 'ISAPRE'},
     {hora: '11:00', especialista: 'MARIA PIA ZAÑARTU',      paciente: 'ANGEL MUÑOZ',    rut: '9878789-2',  prevision: 'ISAPRE'},
     {hora: '11:30', especialista: 'SCARLETT WITTING',       paciente: 'MARIO KAST',     rut: '7998789-5',  prevision: 'FONASA'},
     {hora: '13:00', especialista: 'FRANCISCO VON TEUBER',   paciente: 'KARIN FERNANDEZ',rut: '18887662-K', prevision: 'FONASA'},
     {hora: '13:30', especialista: 'EDUARDO VIÑUELA',        paciente: 'HUGO SANCHEZ',   rut: '17665461-4', prevision: 'FONASA'},
     {hora: '14:00', especialista: 'RAQUEL VILLASECA',       paciente: 'ANA SEPULVEDA',  rut: '14441281-0', prevision: 'ISAPRE'},
 ];
+//desafio 2 punto agregar horas a traumatologia 1.-
+Traumatologia.push(
+    {hora: '09:00',  especialista: 'RENÉ POBLETE', paciente: 'ANA GELLONA',     rut: '13123329-7', prevision: 'ISAPRE'},
+    {hora: '09:30',  especialista: 'MARIA SOLAR', paciente: 'RAMIRO ANDRADE',     rut: '12221451-K', prevision: 'FONASA'},
+    {hora: '10:00',  especialista: 'RAUL LOYOLA', paciente: 'CARMEN ISLA',     rut: '10112348-3', prevision: 'ISAPRE'},
+    {hora: '10:30',  especialista: 'ANTONIO LARENAS', paciente: 'PABLO LOAYZA',     rut: '13123329-7', prevision: 'ISAPRE'},
+    {hora: '12:00',  especialista: 'MATIAS ARAVENA', paciente: 'SUSANA POBLETE',     rut: '14345656-6', prevision: 'FONASA'},
+);
+//oredenar horas traumatologia
+Traumatologia.sort(function (a, b) {
+    if (a.hora > b.hora) {
+      return 1;
+    }
+    if (a.hora < b.hora) {
+      return -1;
+    }
+    return 0;
+  });
+// eliminar primer y ultimo radiologia
+Radiologia.shift();
+Radiologia.pop();
+// imprimir dental
+
+
+var dentales = Dental.reduce(function(acumulador, horas){
+    return acumulador + horas.hora + " - " + horas.especialista + " - " + horas.paciente + " - " + horas.rut + " - " + horas.prevision + "</br>" + "</br>";
+}, "</br> ");
+
+
+
+
+document.write(`<p>Atenciones para Dental: ${dentales}</p>`);
+
+
+
+var titulo = `<h1>Estadisticas centro medico ñuñoa</h1>`
 
 var texto = `
 <tr>
@@ -84,7 +119,8 @@ for(var i = 0; i< Dental.length; i++){
     </tr>
     `;
 }
-document.write('<h1>Estadisticas centro medico ñuñoa</h1>');
+//
+document.getElementById("titulo").innerHTML = titulo
 
 document.getElementById("radiologia").innerHTML = texto
 document.getElementById("traumatologia").innerHTML = texto2
@@ -98,3 +134,7 @@ document.write(`<p>Cantidad de atenciones para Dental: ${Dental.length}</p>`);
 document.write(`<p>Primera atencion: ${Radiologia[0].paciente} - ${Radiologia[0].prevision} | Última atención: ${Radiologia[Radiologia.length -1].paciente} - ${Radiologia[Radiologia.length -1].prevision}.</p>`);
 document.write(`<p>Primera atencion: ${Traumatologia[0].paciente} - ${Traumatologia[0].prevision} | Última atención: ${Traumatologia[Traumatologia.length -1].paciente} - ${Traumatologia[Traumatologia.length -1].prevision}.</p>`);
 document.write(`<p>Primera atencion: ${Dental[0].paciente} - ${Dental[0].prevision} | Última atención: ${Dental[Dental.length -1].paciente} - ${Dental[Dental.length -1].prevision}.</p>`);
+// Desafio 2
+
+
+
